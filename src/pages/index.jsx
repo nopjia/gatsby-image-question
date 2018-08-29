006936ff0ccea8cc757977c3b807f74728711e7c
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-08-29 08:44:10 -0700

    build image gallery index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,45 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const items = data.allItemsJson.edges;
+  return (
+    <div>
+      {items.map(({ node }) => {
+        return (
+          <div
+            key={node.title}
+            style={{
+              display: "inline-block",
+              width: "400px",
+              margin: "15px",
+              padding: "15px",
+              border: "1px solid black"
+            }}
+          >
+            <img
+              width="100%"
+              src={`images/${node.image}`}
+              alt={node.title}
+            />
+            <h3>{node.title}</h3>
+            <p>{node.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query ItemsQuery {
+    allItemsJson {
+      edges {
+        node {
+          title
+          description
+          image
+        }
+      }
+    }
+  }
+`;
